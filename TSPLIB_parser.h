99cd998b7d01115d282f82c82dbd1a391dd3b5d9
Jaitesh Pachipulusu <jaiteshp@gmail.com>
2021-05-04 15:12:24 +0530

    Removed Debug statements

--- TSPLIB_parser.h
+++ TSPLIB_parser.h
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define dbg //cout << __FILE__ << ":" << __LINE__ << ", " << endl
+#define dbg cout << __FILE__ << ":" << __LINE__ << ", " << endl
 
 void allocateMemory(int &n, double **&cost, double *&X, double *&Y) {
     cost = (double **) malloc(sizeof(double*)*n);
@@ -15,14 +15,10 @@ void allocateMemory(int &n, double **&cost, double *&X, double *&Y) {
 
 void ReadFile(string filename, int &n, double **&cost, double *&X, double *&Y) {
     ifstream file;
-    dbg;
 	file.open(filename.c_str());
-    dbg;
     string name, type, EdgeWeightType, EdgeWeightFormat, EdgeDataType, NodeCoordType, DisplayDataType;
-    dbg;
 	if(!file.is_open())
 		throw "Error: could not open instance file";
-    dbg;
 
 	while(!file.eof())
 	{
--- main.cu
+++ main.cu
@@ -3,7 +3,7 @@
 #include <cuda.h>
 
 using namespace std;
-#define dbg //cout << __FILE__ << ":" << __LINE__ << ", " << endl
+#define dbg cout << __FILE__ << ":" << __LINE__ << ", " << endl
 
 int n;
 double **cost;
@@ -11,15 +11,8 @@ double *X, *Y;
 
 int main(int argc, char **argv) {
     string filename = "TSPLIB/";
-    dbg;
     filename = filename + argv[1];
-    dbg;
     cout << filename << endl;
     ReadFile(filename, n, cost, X, Y);
-    dbg;
 
-    for(int i = 0; i < n; i++) {
-        cout << i+1 << "\t" << X[i] << "\t" << Y[i] << endl;
-    }
-    cout << cost[0][1] << "\t" << cost[1][0] << endl;
 }
