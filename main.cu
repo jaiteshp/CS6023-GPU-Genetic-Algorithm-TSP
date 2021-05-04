12e5a98b6ee0ba50f8bcaaad1ad724525ce97bca
Jaitesh Pachipulusu <jaiteshp@gmail.com>
2021-05-05 00:06:59 +0530

    processKernel() written

--- main.cu
+++ main.cu
@@ -6,6 +6,7 @@
 
 using namespace std;
 #define dbg cout << __FILE__ << ":" << __LINE__ << ", " << endl
+#define DBL_MAX 1.7976931348623158e+307
 
 const int POP_SIZE = 100;
 const int NUM_GEN = 1;
@@ -82,10 +83,93 @@ __global__ void copyKernel(int n, int POP_SIZE, int **pop1, int **pop2) {
     return;
 }
 
-__global__ void processKernel(int n, int POP_SIZE, int NUM_MUTATIONS, int **pop1, int **pop2, double **cost, double *X, double *Y, float *rndm) {
+__device__ double computeFitness(int n, int **pop1, int row, double **cost) {
+    double pathLength = 0.0;
+    for(int i = 1; i < n; i++) {
+        int u = pop1[row][i-1];
+        int v = pop1[row][i];
+        pathLength += cost[u][v];
+    }
+    pathLength += cost[pop1[row][n-1]][pop1[row][0]]
+    return pathLength;
+}
+
+__device__ int argMaxFitness(int n, int **pop1, int low, int high, double **cost) {
+    int idx = 0;
+    double mn = DBL_MAX;
+    for(int row = low; row <= high; row++) {
+        double fitness = computeFitness(n, pop1, row, cost);
+        if(fitness < mn) {
+            mn = fitness;
+            idx = row;
+        }
+    }
+    return idx;
+}
+
+__device__ int getAvlblIdx(int &idx, int n, int a, int b) {
+    int res;
+    if(idx < a || idx > b) {
+        res = idx;
+        idx++;
+    } else {
+        res = b+1;
+        idx = b+2;
+    }
+    return res;
+}
+
+__device__ void mutateOffspring(int id, int n, int NUM_MUTATIONS, int **pop2, float *rndm) {
+    int offset = id*(6+2*(NUM_MUTATIONS))+6;
+    for(int mut = 0; mut < NUM_MUTATIONS; mut++) {
+        int a, b;
+        a = n*rndm[offset++];
+        b = n*rndm[offset++];
+
+        int temp = pop2[id][a];
+        pop2[id][a] = pop2[id][b];
+        pop2[id][b] = temp;
+    }
+    return;
+}
+
+__global__ void processKernel(int n, int POP_SIZE, int NUM_MUTATIONS, int **pop1, int **pop2, int **pres, double **cost, double *X, double *Y, float *rndm) {
     int id = (blockIdx.x*blockDim.x)+threadIdx.x;
     if(id >= POP_SIZE) 
         return;
+    int parent1, parent2, low1, high1, low2, high2, a, b;
+    int offset = id*(6+2*(NUM_MUTATIONS));
+    for(int i = 0; i < 6; i++) 
+        rndm[offset+i] = n*rndm[offset+i];
+    low1 = rndm[offset+0];
+    high1 = rndm[offset+1];
+    low2 = rndm[offset+2];
+    high2 = rndm[offset+3];
+
+    parent1 = argMaxFitness(n, pop1, low1, high1, cost);
+    parent2 = argMaxFitness(n, pop1, low2, high2, cost);
+
+    a = rndm[offset+4];
+    b = rndm[offset+5];
+
+    for(int i = 0; i < n; i++) 
+        pres[id][i] = 0;
+    for(int i = a; i <= b; i++) {
+        pop2[id][i] = pop1[parent1][i];
+        pres[id][pop1[parent1][i]] = 1;
+    }
+    int avlblIdx = 0;
+    for(int i = 0; i < n; i++) {
+        int numToInsert = pop1[parent2][i];
+        if(pres[id][numToInsert] == 0) {
+            pres[id][numToInsert] = 1;
+            pop2[id][getAvlblIdx(avlblIdx, n, a, b)] = numToInsert;
+        }
+    }
+
+    mutateOffspring(id, n, NUM_MUTATIONS, pop2, rndm);
+
+    return;    
 }
 
 void generateRandomNumbers() {
